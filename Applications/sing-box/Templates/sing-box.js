19e84ca35aa8e53b827605af55b754f5f24de147
hvmx <xflasher@outlook.com>
2025-04-17 20:06:54 +0800

    sing-box
    v1.11 templates

---
+++ Applications/sing-box/Templates/sing-box.js
@@ -0,0 +1,109 @@
+// https://raw.githubusercontent.com/xream/scripts/main/surge/modules/sub-store-scripts/sing-box/template.js#type=组合订阅&name=机场&outbound=🕳ℹ️all|all-auto🕳ℹ️hk|hk-auto🏷ℹ️港|hk|hongkong|kong kong|🇭🇰🕳ℹ️tw|tw-auto🏷ℹ️台|tw|taiwan|🇹🇼🕳ℹ️jp|jp-auto🏷ℹ️日本|jp|japan|🇯🇵🕳ℹ️sg|sg-auto🏷ℹ️^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)🕳ℹ️us|us-auto🏷ℹ️美|us|unitedstates|united states|🇺🇸
+
+// 示例说明
+// 读取 名称为 "机场" 的 组合订阅 中的节点(单订阅不需要设置 type 参数)
+// 把 所有节点插入匹配 /all|all-auto/i 的 outbound 中(跟在 🕳 后面, ℹ️ 表示忽略大小写, 不筛选节点不需要给 🏷 )
+// 把匹配 /港|hk|hongkong|kong kong|🇭🇰/i  (跟在 🏷 后面, ℹ️ 表示忽略大小写) 的节点插入匹配 /hk|hk-auto/i 的 outbound 中
+// ...
+// 可选参数: includeUnsupportedProxy 包含官方/商店版不支持的协议 SSR. 用法: `&includeUnsupportedProxy=true`
+
+// 支持传入订阅 URL. 参数为 url. 记得 url 需要 encodeURIComponent.
+// 例如: http://a.com?token=123 应使用 url=http%3A%2F%2Fa.com%3Ftoken%3D123
+
+let { type, name, outbound, includeUnsupportedProxy, url } = $arguments
+type = /^1$|col|组合/i.test(type) ? 'collection' : 'subscription'
+let config
+try {
+  config = JSON.parse($content ?? $files[0])
+} catch (e) {
+  throw new Error('配置文件不是合法的 JSON')
+}
+
+let proxies
+if (url) {
+  proxies = await produceArtifact({
+    name,
+    type,
+    platform: 'sing-box',
+    produceType: 'internal',
+    produceOpts: {
+      'include-unsupported-proxy': includeUnsupportedProxy,
+    },
+    subscription: {
+      name,
+      url,
+      source: 'remote',
+    },
+  })
+} else {
+  proxies = await produceArtifact({
+    name,
+    type,
+    platform: 'sing-box',
+    produceType: 'internal',
+    produceOpts: {
+      'include-unsupported-proxy': includeUnsupportedProxy,
+    },
+  })
+}
+
+const outbounds = outbound
+  .split('🕳')
+  .filter(i => i)
+  .map(i => {
+    let [outboundPattern, tagPattern = '.*'] = i.split('🏷')
+    const tagRegex = createTagRegExp(tagPattern)
+    return [outboundPattern, tagRegex]
+  })
+
+config.outbounds.map(outbound => {
+  outbounds.map(([outboundPattern, tagRegex]) => {
+    const outboundRegex = createOutboundRegExp(outboundPattern)
+    if (outboundRegex.test(outbound.tag)) {
+      if (!Array.isArray(outbound.outbounds)) {
+        outbound.outbounds = []
+      }
+      const tags = getTags(proxies, tagRegex)
+      outbound.outbounds.push(...tags)
+    }
+  })
+})
+
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'direct',
+}
+
+let compatible
+config.outbounds.map(outbound => {
+  outbounds.map(([outboundPattern, tagRegex]) => {
+    const outboundRegex = createOutboundRegExp(outboundPattern)
+    if (outboundRegex.test(outbound.tag)) {
+      if (!Array.isArray(outbound.outbounds)) {
+        outbound.outbounds = []
+      }
+      if (outbound.outbounds.length === 0) {
+        if (!compatible) {
+          config.outbounds.push(compatible_outbound)
+          compatible = true
+        }
+        outbound.outbounds.push(compatible_outbound.tag)
+      }
+    }
+  })
+})
+
+config.outbounds.push(...proxies)
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
+
+function createTagRegExp(tagPattern) {
+  return new RegExp(tagPattern.replace('ℹ️', ''), tagPattern.includes('ℹ️') ? 'i' : undefined)
+}
+function createOutboundRegExp(outboundPattern) {
+  return new RegExp(outboundPattern.replace('ℹ️', ''), outboundPattern.includes('ℹ️') ? 'i' : undefined)
+}
