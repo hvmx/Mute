7ae369b53d1271bbe6d86b23ccdd85f21e2650c2
hvmx <>
2025-11-04 23:43:24 +0800

    Uptdate Templates

--- Applications/sing-box/Templates/outbounds.js
+++ Applications/sing-box/Templates/outbounds.js
@@ -21,22 +21,22 @@ config.outbounds.map(i => {
     i.outbounds.push(...getTags(proxies))
   }
   if (['HK'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /香港(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /香港|Hong Kong(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
   if (['JP'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /日本(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /日本|Japan(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
   if (['SG'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /新加坡(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /新加坡|Singapore(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
   if (['US'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /美国(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /美国|America(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
   if (['KR'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /韩国(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /韩国|Korea(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
   if (['TW'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /台湾(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
+    i.outbounds.push(...getTags(proxies, /台湾|Taiwan(?!.*\b(1\.\d+|[2-9]\d*)倍)/))
   }
 })
 
